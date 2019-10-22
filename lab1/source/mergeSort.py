5fa2382d2be268afad839582e3f20320ccbd84a7
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2019-10-23 00:20:27 +0800

    add mergeSort

---
+++ lab1/source/mergeSort.py
@@ -0,0 +1,34 @@
+import sys
+#reange inlcude ben end
+def merge(array, ben, mid, end):
+    if ben >= end:
+        return
+    L = array[ben:mid+1]
+    R = array[mid+1:end+1]
+    L.append(65536)
+    R.append(65536)
+    i = 0
+    j = 0
+    for k in range(ben, end+1):
+        if L[i] <= R[j]:
+            array[k] = L[i]
+            i = i + 1
+        else:
+            array[k] = R[j]
+            j = j + 1
+    return array
+
+def mergeSort(array, beg, end):
+    if beg < end:
+        mid = int((beg+end) / 2)
+        mergeSort(array, beg, mid)
+        mergeSort(array, mid+1, end)
+        merge(array, beg, mid, end)
+        
+    return array
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(15000)
+    a = [18,17,20,11,3100,12,-2,-87]
+    a = mergeSort(a, 0,  len(a)-1)
+    print(a)
