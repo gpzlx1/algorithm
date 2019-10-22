53abd127d463f0546d8755cebdacf7baf100d167
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2019-10-22 23:17:58 +0800

    add countSort

---
+++ lab1/source/countSort.py
@@ -0,0 +1,19 @@
+def countSort(array, k):
+    length = len(array)
+    c = [0] * (k + 1)
+    b = [0] * length
+    for j in array:
+        c[j] = c[j] + 1
+    
+    for i in range(1, k+1):
+        c[i] = c[i] + c[i-1]
+
+    for j in range(length-1, -1, -1):
+        b[c[array[j]] - 1] = array[j]
+        c[array[j]] = c[array[j]] - 1
+
+    return b
+
+if __name__ == "__main__":
+    countSort(a,500)
+    print(a)
